e671759956e6e0ebe7e9b934bec116ef159bf291
swisscheese38 <roland.wehrlin@gmail.com>
2022-12-25 16:17:39 +0100

    Map commands and states

--- catkin_ws/src/rtklm_hardware/src/rtklm_hardware.cpp
+++ catkin_ws/src/rtklm_hardware/src/rtklm_hardware.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <iostream>
+#include <sstream>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
@@ -10,16 +11,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sstream>
 #include <math.h>
 #include <serial/serial.h>
-#define PULSES_PER_REV 1496.0
-#define WHEEL_DIAMETER 0.065
+
+#define PORT "/dev/ttyUSB1"
+#define BAUD_RATE 9600
+#define TIMEOUT 500
 
 namespace rtklm_hardware_interface
 {
 
-    serial::Serial ser;
+    serial::Serial arduino;
+    hardware_interface::JointStateInterface state_interface;
+    hardware_interface::VelocityJointInterface vel_interface;
+    ros::NodeHandle nh;
+    double cmd[2] = {0,0};
+    double pos[2] = {0,0};
+    double vel[2] = {0,0};
+    double eff[2] = {0,0};
 
     RtklmInterface::RtklmInterface()
     {
@@ -27,44 +36,76 @@ namespace rtklm_hardware_interface
 
     RtklmInterface::~RtklmInterface()
     {
+        arduino.close();
     }
 
     bool RtklmInterface::init(ros::NodeHandle &root_nh, ros::NodeHandle &robot_hw_nh)
     {
         try {
-            ser.setPort("/dev/ttyUSB1");
-            ser.setBaudrate(9600);
-            serial::Timeout to = serial::Timeout::simpleTimeout(1000);
-            ser.setTimeout(to);
-            ser.open();
+            arduino.setPort(PORT);
+            arduino.setBaudrate(BAUD_RATE);
+            serial::Timeout to = serial::Timeout::simpleTimeout(TIMEOUT);
+            arduino.setTimeout(to);
+            arduino.open();
         } catch (serial::IOException& e) {
             ROS_ERROR_STREAM("Unable to open serial port");
+            ros::shutdown();
             return false;
         }
 
-        if (ser.isOpen()) {
+        if (arduino.isOpen()) {
             ROS_INFO_STREAM("Serial Port initialized");
         } else {
+            ros::shutdown();
             return false;
         }
+
+        hardware_interface::JointStateHandle left_joint("left_wheel_joint", &pos[0], &vel[0], &eff[0]);
+        state_interface.registerHandle(left_joint);
+
+        hardware_interface::JointStateHandle right_joint("right_wheel_joint", &pos[1], &vel[1], &eff[1]);
+        state_interface.registerHandle(right_joint);
+
+        registerInterface(&state_interface);
+
+        hardware_interface::JointHandle left_vel(state_interface.getHandle("left_wheel"), &cmd[0]);
+        vel_interface.registerHandle(left_vel);
+
+        hardware_interface::JointHandle right_vel(state_interface.getHandle("right_wheel"), &cmd[1]);
+        vel_interface.registerHandle(right_vel);
+
+        registerInterface(&vel_interface);
 
         return true;
     }
 
     void RtklmInterface::read(const ros::Time &time, const ros::Duration &period)
     {
-        if (ser.available()) {
+        if (arduino.available()) {
             ROS_INFO_STREAM("Reading from serial port");
-            std::string result = ser.read(ser.available());
+            std::string result = arduino.read(arduino.available());
             ROS_INFO_STREAM("Read: " << result);
+
+            std::string lpos, rpos, lvel, rvel;
+            std::stringstream sstream;
+
+            sstream.str(result);
+            std::getline(sstream, lpos, ' ');
+            std::getline(sstream, lvel, ' ');
+            std::getline(sstream, rpos, ' ');
+            std::getline(sstream, rvel, ' ');
+            pos[0] = stod(lpos);
+            vel[0] = stod(lvel);
+            pos[1] = stod(rpos);
+            vel[1] = stod(rvel);
         }
     }
 
     void RtklmInterface::write(const ros::Time &time, const ros::Duration &period)
     {
-        std::string command = "0.0 0.0";
+        std::string command = std::to_string(cmd[0]) + " " + std::to_string(cmd[1]);
         ROS_INFO_STREAM("Writing to serial port" << command);
-        ser.write(command);
+        arduino.write(command);
     }
 
 } // namespace rtklm_hardware_interface
