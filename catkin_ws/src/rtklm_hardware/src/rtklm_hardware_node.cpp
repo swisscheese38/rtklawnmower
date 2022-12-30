587c4f831de92b92c3498f811a9ecc93066ca2a9
swisscheese38 <roland.wehrlin@gmail.com>
2022-12-30 20:19:13 +0000

    First tries with ROS on real hardware

--- catkin_ws/src/rtklm_hardware/src/rtklm_hardware_node.cpp
+++ catkin_ws/src/rtklm_hardware/src/rtklm_hardware_node.cpp
@@ -1,31 +1,100 @@
-#include <iostream>
+#include <math.h>
 #include <ros/ros.h>
+#include <std_msgs/Float64.h>
 #include <controller_manager/controller_manager.h>
-#include "rtklm_hw.hpp"
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+using namespace std;
+
+class RtklmInterface : public hardware_interface::RobotHW
+{
+public:
+  RtklmInterface(string port, unsigned long baud) {
+     
+    pos_[0] = 0.0; pos_[1] = 0.0;
+    vel_[0] = 0.0; vel_[1] = 0.0;
+    eff_[0] = 0.0; eff_[1] = 0.0;
+    cmd_[0] = 0.0; cmd_[1] = 0.0;
+
+    hardware_interface::JointStateHandle state_handle_1("left_wheel_joint", &pos_[0], &vel_[0], &eff_[0]);
+    jnt_state_interface_.registerHandle(state_handle_1);
+
+    hardware_interface::JointStateHandle state_handle_2("right_wheel_joint", &pos_[1], &vel_[1], &eff_[1]);
+    jnt_state_interface_.registerHandle(state_handle_2);
+
+    registerInterface(&jnt_state_interface_);
+
+    hardware_interface::JointHandle vel_handle_1(jnt_state_interface_.getHandle("left_wheel_joint"), &cmd_[0]);
+    jnt_vel_interface_.registerHandle(vel_handle_1);
+
+    hardware_interface::JointHandle vel_handle_2(jnt_state_interface_.getHandle("right_wheel_joint"), &cmd_[1]);
+    jnt_vel_interface_.registerHandle(vel_handle_2);
+
+    registerInterface(&jnt_vel_interface_);
+  }
+
+  ~RtklmInterface(){
+    //
+  }
+
+  ros::Time getTime() const {return ros::Time::now();}
+  ros::Duration getPeriod() const {return ros::Duration(0.01);}
+
+  void read(){
+    ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
+    //send commands to motors
+    //K1.s((int32_t)  cmd_[0] * radiansToTicks);
+    //K2.s((int32_t) -cmd_[1] * radiansToTicks);
+  }
+
+  void write(){
+    //pos_[0] =  resultGetK1P.value() * ticksToRadians;
+    //pos_[1] = -resultGetK2P.value() * ticksToRadians;
+    //vel_[0] =  K1.getS().value() * ticksToRadians;
+    //vel_[1] = -K2.getS().value() * ticksToRadians;
+  }
+
+private:
+  hardware_interface::JointStateInterface    jnt_state_interface_;
+  hardware_interface::VelocityJointInterface jnt_vel_interface_;
+  double ticksToRadians;
+  double radiansToTicks;
+  double cmd_[2];
+  double pos_[2];
+  double vel_[2];
+  double eff_[2];
+
+};
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "rtklm_hardware_control_node");
+  double x, y, theta;
+  
+  ros::init(argc, argv, "rtklm_hardware_node");
+  ros::NodeHandle nh;
+ 
+ //TODO: read port and baud as parameters
+  string port = "/dev/ttyUSB0";
+  unsigned long baud = 9600;
 
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    ros::NodeHandle nh;
-    rtklm_hardware_interface::RtklmInterface hw;
-    bool init_success = hw.init(nh, nh);
+  RtklmInterface robot(port, baud);
+  ROS_INFO_STREAM("period: " << robot.getPeriod().toSec());
+  controller_manager::ControllerManager cm(&robot, nh);
 
-    controller_manager::ControllerManager cm(&hw, nh);
+  ros::Rate rate(1.0 / robot.getPeriod().toSec());
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
-    ros::Rate rate(200); // 200Hz update rate
+  while(ros::ok())
+  {
+    //TODO: Detect loss of connection to robot and reconnect
+    robot.read();
+    robot.write();
+    cm.update(robot.getTime(), robot.getPeriod());
+    rate.sleep();
+  }
+  spinner.stop();
 
-    ROS_INFO("rtklm_hardware_control started");
-    while (ros::ok())
-    {
-        hw.read(ros::Time::now(), rate.expectedCycleTime());
-        cm.update(ros::Time::now(), rate.expectedCycleTime());
-        hw.write(ros::Time::now(), rate.expectedCycleTime());
-        rate.sleep();
-    }
-
-    spinner.stop();
-    return 0;
+  return 0;
 }
